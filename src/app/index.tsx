a214c7e490baa2ac36f7d63dd2af59334c07b489
Luilu-star <luiotv2302@gmail.com>
2026-05-08 19:13:56 -0300

    Remasterizando login screen - part 1

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,6 @@ import { StyleSheet, Text, TextInput, View, Image, Button, TouchableOpacity } fr
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
     },
     background: {
         flex: 1,
@@ -17,7 +15,15 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: "bold",
     },
-    card: {
+    separator: {
+        width: '100%',
+        height: 1,
+        backgroundColor: '#ccc',
+        marginVertical: 15,
+        borderBottomWidth: 3,
+        borderBottomColor: '#ccc',
+    },
+    /*card: {
         width: '90%',
         height: '70%',
         backgroundColor: '#f7f7f7',
@@ -30,15 +36,8 @@ const styles = StyleSheet.create({
         alignContent: 'center',
         
     },
-    separator: {
-        width: '100%',
-        height: 1,
-        backgroundColor: '#ccc',
-        marginVertical: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-    },
-    /*form: {
+    
+    form: {
         justifyContent: 'flex-start',
         alignContent: 'flex-start',
         marginTop: 20,
@@ -51,21 +50,31 @@ export default function App() {
     return (
         <>
         <View style={styles.container}>
-            
-                <View style={styles.card}>
-                    <Image source={require('@/assets/logo.png')} style={{marginTop: 20, alignSelf: 'center'}} />
-                    <Text style={[styles.Text, {marginTop: 50}]}>Login</Text>
-                    <TextInput placeholder="Matricula" style={{marginTop: 40, padding: 10, borderWidth: 1, borderColor: '#ccc', borderRadius: 5}} />
-                    <TextInput placeholder="Senha" secureTextEntry={true} style={{marginTop: 20, padding: 10, borderWidth: 1, borderColor: '#ccc', borderRadius: 5}} />
-                    
-                    <TouchableOpacity>
-                        <Text style={{color: '#007BFF', marginTop: 10, marginBottom: 120, marginRight: 10, alignSelf: 'flex-end'}}>Esqueci minha senha</Text>
-                    </TouchableOpacity>
-        
-                    <Button title="Entrar"  color={'#B8E891'} />
-                    <View style={styles.separator} />
 
-                </View>
+            <View></View>
+        
+            <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 50, alignSelf: 'center'}}> 
+                <Text style={{fontWeight: 700, fontSize: 45, color: '#2BB846'}}>LO</Text><Text style={{ fontWeight: 700, fontSize: 45, color: '#CC191E'}}>GIN</Text>
+            </View>
+
+            <View style={styles.separator} />
+
+            <View style={{padding: 30,}}>
+
+                <Text style={{marginTop: 70, marginBottom: 10, fontWeight: 'bold', fontSize: 20}}>Matricula</Text>
+                <TextInput placeholder="Matricula" style={{padding: 13, borderRadius: 12, backgroundColor: '#D9D9D9'}} />
+
+                <Text style={{marginTop: 35, marginBottom: 10, fontWeight: 'bold', fontSize: 20}}>Senha</Text>
+                <TextInput placeholder="Senha" secureTextEntry={true} style={{padding: 13, borderRadius: 12, backgroundColor: '#D9D9D9'}} />
+                
+                <TouchableOpacity>
+                    <Text style={{color: '#007BFF', marginTop: 10, marginBottom: 120, marginRight: 10, alignSelf: 'flex-end'}}>Esqueci minha senha</Text>
+                </TouchableOpacity>
+
+                <Button title="Entrar"  color={'#B8E891'} />
+
+            </View>
+
 
         </View>
         </>
