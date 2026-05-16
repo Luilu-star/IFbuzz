6ffe7e32d6b2736dc8a3d51dd49bb2fa8a3654cf
Luilu-star <luiotv2302@gmail.com>
2026-05-16 16:06:31 -0300

    Modificações tela de cadastro (icon + router de volta)

--- src/app/telaCadastro.tsx
+++ src/app/telaCadastro.tsx
@@ -1,7 +1,8 @@
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { defaultConfig } from '@tamagui/config/v5';
-import { Text, View } from "react-native";
+import { Text, View, Image} from "react-native";
+import { useRouter } from 'expo-router';
 import { Button, createTamagui, TamaguiProvider } from 'tamagui';
 import "../../global.css";
 
@@ -10,11 +11,43 @@ const config = createTamagui(defaultConfig)
 
 export default function telaCadastro() {
 
+    const router = useRouter();
+
+    function Voltar(){
+        router.push("/");
+    }
     return (
         <TamaguiProvider config={config} defaultTheme={'light'}>
 
-            <View className='flex-1 p-5 bg-white'>
-                <Button size={'$4'} width={60} background={'#FFFFFF'}><FontAwesomeIcon size={30} icon={faArrowLeft} /></Button>
+            <View className='flex-1 pt-10 bg-white'>
+
+                <View className='ml-8 mt-5'>
+                    <Button onPress={Voltar} unstyled={true}><FontAwesomeIcon size={30} icon={faArrowLeft} /></Button>
+                </View>
+
+                <View style={{
+                    width: 150,
+                    height: 150,
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: 6 },
+                    shadowOpacity: 0.3,
+                    shadowRadius: 10,
+                    elevation: 8,
+                }} className='self-center mt-[50px] border-2 rounded-[100%] bg-white justify-center'>
+                    <Image className='self-center' source={require('@/assets/estudante.png')}></Image>
+                </View>
+
+                <View style={{width:'100%', height:'60%',
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: -2 },
+                    shadowOpacity: 0.5,
+                    shadowRadius: 5,
+                    elevation: 1,}} className='bg-white mt-[75px] border shadow rounded-[40px]'>
+
+                </View>
+
+                
+                
                 
             </View>
 
