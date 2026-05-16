0dc94fbd12c3ea37a766b022b625448aaeb5355a
Luilu-star <luiotv2302@gmail.com>
2026-05-16 13:53:24 -0300

    Adição de biblioteca FontAwesome

--- src/app/telaCadastro.tsx
+++ src/app/telaCadastro.tsx
@@ -1,26 +1,23 @@
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { defaultConfig } from '@tamagui/config/v5';
+import { Text, View } from "react-native";
+import { Button, createTamagui, TamaguiProvider } from 'tamagui';
 import "../../global.css";
-import { useState } from "react";
-import { View, Text, Button } from "react-native";
-import { Cat } from '@/components/Cat';
-import { createTamagui, TamaguiProvider} from 'tamagui'
-import { defaultConfig } from '@tamagui/config/v5'
+
 
 const config = createTamagui(defaultConfig)
 
-
 export default function telaCadastro() {
-
-    const [isHungry, setIsHungry] = useState(true);
 
     return (
         <TamaguiProvider config={config} defaultTheme={'light'}>
 
-            <View className="flex-1 justify-center items-center">
-                <Text className="text-[24px] font-bold">Essa é a tela de cadastro!</Text>
-
-                <Cat nome={'Frajola'} idade={3} cor={'preto e branco'}/>
-                                <Button onPress={() => setIsHungry(false)} disabled={!isHungry} title={isHungry ? "Quero comer" : "Obregado"} />
+            <View className='flex-1 p-5 bg-white'>
+                <Button size={'$4'} width={60} background={'#FFFFFF'}><FontAwesomeIcon size={30} icon={faArrowLeft} /></Button>
+                
             </View>
+
 
         </TamaguiProvider>
         
