81b6e2a430ee6a421270ae1f969148f6474709d3
Luilu-star <luiotv2302@gmail.com>
2026-06-26 09:04:49 -0300

    Adição da tela de presenças

---
+++ src/components/modules/presencas.ts
@@ -0,0 +1,28 @@
+import { PresençaItem } from './card';
+
+export const presencas: PresençaItem[] = [
+  {
+    id: '1',
+    rota: 'Cedro - Lavras',
+    data: '02/03/26',
+    turno: 'Manhã',
+    motorista: 'João Silva',
+    situacao: 'Presente',
+  },
+  {
+    id: '2',
+    rota: 'Assunção - Centro',
+    data: '02/03/26',
+    turno: 'Tarde',
+    motorista: 'Maria Souza',
+    situacao: 'Presente',
+  },
+  {
+    id: '3',
+    rota: 'Lavras - Centro',
+    data: '03/03/26',
+    turno: 'Noite',
+    motorista: 'Carlos Oliveira',
+    situacao: 'Presente',
+  },
+]
